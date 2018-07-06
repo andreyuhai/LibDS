8d5233f33df5e77fad60e129e943272de23c487c
Burak Kaymakci <yuhai.ndre@gmail.com>
2018-07-07 00:13:08 +0300

    Test files

---
+++ Tests/Test_Node.c
@@ -0,0 +1,19 @@
+#include "../Headers/Foundation.h"
+
+void Test_Node()
+{
+    Node * node     = Node_Manager.create();
+    Node * node1    = Node_Manager.create();
+
+    node->data_ptr  = malloc(6*sizeof(char));
+    node1->data_ptr = malloc(6*sizeof(char));
+
+    strcpy(node->data_ptr,"Andre");
+    strcpy(node1->data_ptr,"Yuhai");
+
+    printf("Name\t: %s\n",(char *)node->data_ptr);
+    printf("Surname\t: %s\n",(char *)node1->data_ptr);
+
+    Node_Manager.destroy(node);
+    Node_Manager.destroy(node1);
+}
