34c6f04738886dfcd5e7ed7b78c42832e8b2694d
Burak Kaymakci <yuhai.ndre@gmail.com>
2018-07-07 23:26:21 +0300

    Node deletion test added.

--- Headers/Tests.h
+++ Headers/Tests.h
@@ -2,8 +2,10 @@
 #define LIBDS_TESTS_H
 
 void Test_Node();
-void Test_Append();
+
+void Test_LinkedList_Append();
 void Test_LinkedList_Create();
 void Test_LinkedList_wData();
+void Test_LinkedList_Deletion();
 
 #endif //LIBDS_TESTS_H
