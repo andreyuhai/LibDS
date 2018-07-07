53884eb07ec6798e00954cada3f459d86adc53bb
Burak Kaymakci <yuhai.ndre@gmail.com>
2018-07-07 22:03:04 +0300

    One for all.

---
+++ Headers/Base.h
@@ -0,0 +1,10 @@
+#ifndef LIBDS_BASE_H
+#define LIBDS_BASE_H
+
+#include "SystemIncludes.h"
+#include "Helpers.h"
+#include "Tests.h"
+#include "Node.h"
+#include "LinkedList.h"
+
+#endif //LIBDS_BASE_H
