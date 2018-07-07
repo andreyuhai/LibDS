937135469a86f3240a50a7de6edd4040aa0c62e7
Burak Kaymakci <yuhai.ndre@gmail.com>
2018-07-07 22:03:24 +0300

    Linked lists implemented.

---
+++ Headers/LinkedList.h
@@ -0,0 +1,20 @@
+#ifndef LIBDS_LINKEDLIST_H
+#define LIBDS_LINKEDLIST_H
+
+#include "Base.h"
+
+typedef struct LinkedListManager LinkedListManager;
+
+struct LinkedListManager
+{
+    Node *  (* create)      (size_t number_of_nodes);
+    void    (* append)      (Node * root, Node * tail);
+    size_t  (* size)        (Node * root);
+    void    (* destroy)     (Node * root);
+    void    (* deleteNode)  (size_t node_number);
+
+};
+
+const LinkedListManager LinkedList_Manager;
+
+#endif //LIBDS_LINKEDLIST_H
